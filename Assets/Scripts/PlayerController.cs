7bf0e5134dcc633136aa590e6969af6629fa853a
Andy Pang <cp487@cornell.edu>
2024-07-05 09:20:03 -0400

    Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float groundDistance;
+
+    public LayerMask groundLayer;
+    public SpriteRenderer sr;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 castPos = transform.position;
+        castPos.y += 1;
+        if (Physics.Raycast(castPos, -transform.up, out RaycastHit hit, Mathf.Infinity, groundLayer)) {
+            if (hit.collider != null) {
+                Vector3 movePos = transform.position;
+                movePos.y = hit.point.y + groundDistance;
+                transform.position = movePos;
+            }
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = new Vector3(x, 0, y);
+        rb.velocity = moveDir.normalized * speed;
+
+        if (x != 0 && x < 0) {
+            sr.flipX = true;
+        }
+
+        else if (x != 0 && x > 0) {
+            sr.flipX = false;
+        }
+    }
+}
