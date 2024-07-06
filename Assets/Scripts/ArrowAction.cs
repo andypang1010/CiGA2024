390700e53d760257caa3f81f22562b6082ef8f6e
George Zou <porridgeorge@gmail.com>
2024-07-06 13:07:01 -0400

    arrow triggers player death

---
+++ Assets/Scripts/ArrowAction.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ArrowAction : MonoBehaviour
+{
+    public LayerMask playerLayer; // Layer to detect the player
+    public GameObject player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the collided object is on the player layer
+        if (((1 << other.gameObject.layer) & playerLayer) != 0)
+        {
+            Debug.Log("Arrow hit the player!");
+            if (other.gameObject.name == "PLAYER")
+            {
+                Debug.Log("REAL PLAYER!");
+                other.gameObject.GetComponent<PlayerDeath>().Die();
+            }
+        }
+    }
+}
